fb12f370252576bf821b6aac591d027c0d6b4910
ALALASUNDARAM SARAVANAN <saraindus@yahoo.com>
2023-05-28 18:37:20 +0530

    Add project files.

---
+++ HAPLC/Form1.Designer.cs
@@ -0,0 +1,211 @@
+﻿namespace HAPLC
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.BtnConnect = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.txtWrite = new System.Windows.Forms.TextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.winsock2 = new MelsecPLC.Winsock();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtPLCIPAddress = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.btnRead = new System.Windows.Forms.Button();
+            this.btnWrite = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // BtnConnect
+            // 
+            this.BtnConnect.Location = new System.Drawing.Point(241, 30);
+            this.BtnConnect.Name = "BtnConnect";
+            this.BtnConnect.Size = new System.Drawing.Size(87, 46);
+            this.BtnConnect.TabIndex = 0;
+            this.BtnConnect.Text = "PLC Connect";
+            this.BtnConnect.UseVisualStyleBackColor = true;
+            this.BtnConnect.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(119, 62);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(116, 21);
+            this.textBox1.TabIndex = 1;
+            // 
+            // txtWrite
+            // 
+            this.txtWrite.Location = new System.Drawing.Point(119, 94);
+            this.txtWrite.Name = "txtWrite";
+            this.txtWrite.Size = new System.Drawing.Size(116, 21);
+            this.txtWrite.TabIndex = 2;
+            this.txtWrite.Text = "0002";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // winsock2
+            // 
+            this.winsock2.LocalPort = 80;
+            this.winsock2.RemoteIP = "127.0.0.1";
+            this.winsock2.RemotePort = 80;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(22, 65);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(91, 12);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "PLC Read Data";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(22, 34);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(95, 12);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "PLC IP Address";
+            // 
+            // txtPLCIPAddress
+            // 
+            this.txtPLCIPAddress.Location = new System.Drawing.Point(119, 31);
+            this.txtPLCIPAddress.Name = "txtPLCIPAddress";
+            this.txtPLCIPAddress.Size = new System.Drawing.Size(116, 21);
+            this.txtPLCIPAddress.TabIndex = 4;
+            this.txtPLCIPAddress.Text = "10.126.224.221";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(22, 97);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(89, 12);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "PLC Write Data";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(241, 82);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(87, 33);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Disconnect";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // btnRead
+            // 
+            this.btnRead.Location = new System.Drawing.Point(12, 130);
+            this.btnRead.Name = "btnRead";
+            this.btnRead.Size = new System.Drawing.Size(145, 37);
+            this.btnRead.TabIndex = 8;
+            this.btnRead.Text = "Read Data from PLC";
+            this.btnRead.UseVisualStyleBackColor = true;
+            this.btnRead.Click += new System.EventHandler(this.btnRead_Click);
+            // 
+            // btnWrite
+            // 
+            this.btnWrite.Location = new System.Drawing.Point(163, 130);
+            this.btnWrite.Name = "btnWrite";
+            this.btnWrite.Size = new System.Drawing.Size(147, 37);
+            this.btnWrite.TabIndex = 9;
+            this.btnWrite.Text = "Write Data to PLC";
+            this.btnWrite.UseVisualStyleBackColor = true;
+            this.btnWrite.Click += new System.EventHandler(this.btnWrite_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(70, 183);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(175, 12);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "syedshanumcain@gmail.com";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::HAPLC.Properties.Resources._611;
+            this.pictureBox1.Location = new System.Drawing.Point(334, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(200, 197);
+            this.pictureBox1.TabIndex = 11;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(543, 218);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.btnWrite);
+            this.Controls.Add(this.btnRead);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtPLCIPAddress);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtWrite);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.BtnConnect);
+            this.Name = "Form1";
+            this.Text = "Shanu PLC Communication Program";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BtnConnect;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox txtWrite;
+        private System.Windows.Forms.Timer timer1;
+        private MelsecPLC.Winsock winsock2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtPLCIPAddress;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnRead;
+        private System.Windows.Forms.Button btnWrite;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
